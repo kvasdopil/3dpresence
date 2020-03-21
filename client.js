4623f6bfaed5337a1f2871e89dfe3ce1ca4aaee1
Alexey Guskov <lexag@mail.ru>
2020-03-21 15:25:25 +0100

    client cleanup

--- client.js
+++ client.js
@@ -11,12 +11,8 @@ const initClientStream = () => {
 
 const client = async () => {
     const remoteVideo = document.getElementById('remote-video');
-    const globalChannel = 'global-channel';
+    const channel = 'global-channel';
     let webRtcPhone;
-    // An RTCConfiguration dictionary from the browser WebRTC API
-    // Add STUN and TURN server information here for WebRTC calling
-    const rtcConfig = {};
-    const username = 'client';
 
     let noVideoTimeout; // Used to check if a video connection succeeded
     const noVideoTimeoutMS = 5000; // Error alert if the video fails to connect
@@ -27,53 +23,35 @@ const client = async () => {
         if (remoteVideo.paused) {
             alert(message);
             clearTimeout(noVideoTimeout);
-            webRtcPhone.disconnect(); // disconnects the current phone call
+            webRtcPhone.disconnect();
         }
     }
 
     const myAudioVideoStream = await initClientStream();
 
-    // WebRTC phone object event for when the remote peer's video becomes available.
     const onPeerStream = (webRTCTrackEvent) => {
-        console.log('Peer audio/video stream now available');
-        const peerStream = webRTCTrackEvent.streams[0];
-        // window.peerStream = peerStream;
-        remoteVideo.srcObject = peerStream;
+        remoteVideo.srcObject = webRTCTrackEvent.streams[0];
     };
 
-    // WebRTC phone object event for when the remote peer responds to your call request.
     const onCallResponse = (acceptedCall) => {
         if (acceptedCall) {
             noVideoTimeout = setTimeout(noVideo, noVideoTimeoutMS);
         }
     };
-    // WebRTC phone object event for when a call disconnects or timeouts.
+
     const onDisconnect = () => {
         clearTimeout(noVideoTimeout);
     };
-    // Lists the online users in the UI and registers a call method to the click event
-    //     When a user clicks a peer's name in the online list, the app calls that user.
-    const addToOnlineUserList = (occupant) => {
-        if (occupant.state && occupant.state.name === 'server') {
-            webRtcPhone.callUser(occupant.uuid, {
-                myStream: myAudioVideoStream
-            });
-        }
-    }
 
     const onInit = (status, response) => {
-        response.channels[globalChannel].occupants.forEach(addToOnlineUserList);
-    }
-
-    const onPresence = (status, response) => {
-        if (status.error) {
-            console.error(status.error);
-        } else if (status.channel === globalChannel) {
-            if (status.action === "join" || status.action === "state-change") {
-                addToOnlineUserList(status, response);
+        response.channels[channel].occupants.forEach((occupant) => {
+            if (occupant.state && occupant.state.name === 'server') {
+                webRtcPhone.callUser(occupant.uuid, {
+                    myStream: myAudioVideoStream
+                });
             }
-        }
-    };
+        });
+    }
 
     const pubnub = new PubNub({
         publishKey: 'pub-c-27f55276-cf45-4677-886f-9c30d1c361ff',
@@ -85,33 +63,33 @@ const client = async () => {
             if (statusEvent.category === "PNConnectedCategory") {
                 pubnub.setState({
                     state: {
-                        name: username
+                        name: 'client',
                     },
-                    channels: [globalChannel],
+                    channels: [channel],
                     uuid: pubnub.getUUID()
                 });
                 pubnub.hereNow({
-                    channels: [globalChannel],
+                    channels: [channel],
                     includeUUIDs: true,
                     includeState: true
                 }, onInit)
             }
         },
-        presence: onPresence
+        presence: () => { },
     });
     pubnub.subscribe({
-        channels: [globalChannel],
+        channels: [channel],
         withPresence: true
     });
     window.ismyuuid = pubnub.getUUID();
     window.onbeforeunload = (event) => {
         pubnub.unsubscribe({
-            channels: [globalChannel]
+            channels: [channel]
         });
     };
 
     webRtcPhone = new WebRtcPhone({
-        rtcConfig,
+        rtcConfig: {},
         ignoreNonTurn: false,
         myStream: myAudioVideoStream,
         onPeerStream,   // is required
